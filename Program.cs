5a7a727ab0fde3db2be7757ccca650ad0073f7de
Vitalii Kertanov <RetalityAWD@yandex.ru>
2024-12-08 14:56:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pattern_abstract_factory_ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
